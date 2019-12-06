4d2dc6c7cb9cc3fde3578381de4f2c3d94f42b89
Leray Loic <loic.leray@etu.u-bordeaux.fr>
2019-12-06 01:23:49 +0100

    rotate cle

--- felindra/js/script.js
+++ felindra/js/script.js
@@ -5,38 +5,31 @@ $(document).ready(function () {
         $("#footer").toggleClass("footerColor footerBlackWhite");
     });
 
-    $("#change_font").click(function() {
 
-      var size = $("p").css( "font-size" );
-      alert("Size="+size)
-      $("p").css( "font-size" , size+10 );
-
-    });
-
-
-    var originalSize = $('div').css('font-size');
+    var originalSize = $('*').css('font-size');
+    var originalSize = parseFloat(originalSize)*1.5;
+    $('*').css('font-size', originalSize);
       // reset
      $(".resetMe").click(function(){
-       $('div').css('font-size', originalSize);
+       $('*').css('font-size', originalSize);
      });
 
      // Increase Font Size
      $(".increase").click(function(){
-      var currentSize = $('div').css('font-size');
+      var currentSize = $('*').css('font-size');
       var currentSize = parseFloat(currentSize)*1.2;
-      $('div').css('font-size', currentSize);
+      $('*').css('font-size', currentSize);
       return false;
     });
 
      // Decrease Font Size
      $(".decrease").click(function(){
-       var currentFontSize = $('div').css('font-size');
-       var currentSize = $('div').css('font-size');
+       var currentFontSize = $('*').css('font-size');
+       var currentSize = $('*').css('font-size');
        var currentSize = parseFloat(currentSize)*0.8;
-       $('div').css('font-size', currentSize);
+       $('*').css('font-size', currentSize);
        return false;
      });
-
 
 
 });
